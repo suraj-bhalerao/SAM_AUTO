fb040254d9bfb3303c6be2cf212713e40d79aa0c
surajB2024 <surajbhalerao2024@gmail.com>
2025-02-21 15:36:50 +0530

    Changed

--- src/main/java/com/aepl/sam/locators/GovernmentServerPageLocators.java
+++ src/main/java/com/aepl/sam/locators/GovernmentServerPageLocators.java
@@ -1,5 +1,13 @@
 package com.aepl.sam.locators;
 
-public class GovernmentServerPageLocators {
+import org.openqa.selenium.By;
 
+public class GovernmentServerPageLocators {
+	
+	// Dropdown Link
+	public static final By GOVERNMENT_DRP_LINK = By.xpath("//a[@routerlink='govt-servers' and @href='/govt-servers']");
+	
+	// Page Headers Links
+	public static final By BACK_BTN = By.className("action-button back-button");
+	public static final By REFRESH_BTN = By.className("action-button reload-button");
 }
