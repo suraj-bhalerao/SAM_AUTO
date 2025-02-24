0b5e3ddfd3a23c7421245c07285b316373058d04
shital <sshingare3@gmail.com>
2025-02-24 11:13:00 +0530

    sdas

--- src/main/java/com/aepl/sam/locators/DeviceDashboardPageLocators.java
+++ src/main/java/com/aepl/sam/locators/DeviceDashboardPageLocators.java
@@ -1,5 +1,11 @@
 package com.aepl.sam.locators;
 
+import org.openqa.selenium.By;
+
 public class DeviceDashboardPageLocators {
-	// Sharukh
+	// Shital
+	
+	//	navigation bar
+	public static final By dashboard = By.xpath("/html/body/app-root/app-header/div/div/nav/ul/li[1]/a");
+	
 }
--- src/main/java/com/aepl/sam/locators/GovernmentServerPageLocators.java
+++ src/main/java/com/aepl/sam/locators/GovernmentServerPageLocators.java
@@ -1,5 +1,10 @@
 package com.aepl.sam.locators;
 
 public class GovernmentServerPageLocators {
+	
+	
+	
+	
+	
 
 }
