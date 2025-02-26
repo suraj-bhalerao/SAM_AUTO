07bad16ec92452b078d46b2273d763507c26d709
shital <sshingare3@gmail.com>
2025-02-26 12:37:54 +0530

    Add new chages in Device Dashboard by Shital

--- src/main/java/com/aepl/sam/locators/DeviceDashboardPageLocators.java
+++ src/main/java/com/aepl/sam/locators/DeviceDashboardPageLocators.java
@@ -6,6 +6,21 @@ public class DeviceDashboardPageLocators {
 	// Shital
 	
 	//	navigation bar
-	public static final By dashboard = By.xpath("/html/body/app-root/app-header/div/div/nav/ul/li[1]/a");
+	public static final By device_dashboard = By.xpath("/html/body/app-root/app-header/div/div/nav/ul/li[1]/a");
+	public static final By device_dashboard_backbtn = By.xpath("/html/body/app-root/app-device-dashboard/div/form/div[1]/div/div[1]/mat-icon");
+	public static final By device_dashboard_refreshbtn = By.xpath("/html/body/app-root/app-device-dashboard/div/form/div[1]/div/div[2]/mat-icon");
+	public static final By device_dashboard_pagetitle = By.xpath("/html/body/app-root/app-device-dashboard/div/form/div[1]/span");
+	public static final By device_dashboard_navbar = By.xpath("/html/body/app-root/app-header/div/div/nav/ul/li[1]/a");
+	public static final By device_dashboard_navbar_deviceutility = By.xpath("/html/body/app-root/app-header/div/div/nav/ul/li[2]/a");
+	public static final By device_dashboard_navbar_user = By.xpath("/html/body/app-root/app-header/div/div/nav/ul/li[3]/a");
+	public static final By device_dashboard_navbar_userprofile = By.xpath("/html/body/app-root/app-header/div/div/nav/ul/li[4]/a/span");
+	public static final By device_dashboard_TotalProductionDevicesKPI = By.id("TotalProductionDevices");
+	public static final By device_dashboard_TotalDispatchedDevicesKPI = By.id("DispatchedDevices");
+	public static final By device_dashboard_ProductionDevicesTableHeader = By.xpath("/html/body/app-root/app-device-dashboard/div/form/div[4]/form/div[1]/div/div/div[1]/div/h6");
+	public static final By device_dashboard_DispatchedDevicesTableHeader = By.xpath("/html/body/app-root/app-device-dashboard/div/form/div[4]/form/div[1]/div/div/div[1]/div/h6");
+	public static final By device_dashboard_searchbox = By.xpath("/html/body/app-root/app-device-dashboard/div/form/div[4]/form/div[1]/div/div/div[3]/app-common-component-search/div/div/div/input");
+	public static final By device_dashboard_searchclick = By.xpath("/html/body/app-root/app-device-dashboard/div/form/div[4]/form/div[1]/div/div/div[3]/app-common-component-search/div/div/button");
+	public static final By device_dashboard_searchclear = By.xpath("/html/body/app-root/app-device-dashboard/div/form/div[4]/form/div[1]/div/div/div[3]/app-common-component-search/div/div/div/input");
+	
 	
 }
--- src/main/java/com/aepl/sam/pages/DeviceDashboardPage.java
+++ src/main/java/com/aepl/sam/pages/DeviceDashboardPage.java
@@ -1,5 +1,29 @@
 package com.aepl.sam.pages;
 
-public class DeviceDashboardPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.aepl.sam.locators.DeviceDashboardPageLocators;
+import com.aepl.sam.utils.CommonMethods;
+
+public class DeviceDashboardPage extends DeviceDashboardPageLocators {
+	
+//	public class DeviceDashboardPage extends DeviceDashboardPageLocators {
+
+		private WebDriver driver;
+		private WebDriverWait wait;
+		private CommonMethods commonMethods;
+
+		public DeviceDashboardPage(WebDriver driver, WebDriverWait wait) {
+			this.driver = driver;
+			this.wait = wait;
+			this.commonMethods = new CommonMethods(driver);
+		}
+
+		public void clickOnNavBar() {
+			WebElement navBarLink = wait.until(ExpectedConditions.visibilityOfElementLocated(device_dashboard));
+			navBarLink.click();
+		}
 }
