11d7896ceb0b27a5eca5124d93bff46dfa9ff8b5
surajB2024 <surajbhalerao2024@gmail.com>
2025-02-28 10:28:18 +0530

    Suraj: User management Locators

--- src/main/java/com/aepl/sam/locators/CommonLocatorsPage.java
+++ src/main/java/com/aepl/sam/locators/CommonLocatorsPage.java
@@ -3,6 +3,7 @@ package com.aepl.sam.locators;
 public class CommonLocatorsPage {
 
 	// Main Nav Bar Section
+	
 	// Header Section
 	// Pagination Section
 	// Footer Section
--- src/main/java/com/aepl/sam/locators/UserManagementPageLocators.java
+++ src/main/java/com/aepl/sam/locators/UserManagementPageLocators.java
@@ -1,5 +1,22 @@
 package com.aepl.sam.locators;
 
+import org.openqa.selenium.By;
+
 public class UserManagementPageLocators {
 
+	// User Management
+	public static final By USR_MANAGEMENT_LINK = By.xpath("//a[@routerlink='user-tab' and @href='/user-tab']");
+	public static final By ADD_USR_BTN = By.xpath("//button[contains(text(), 'Add')]");
+	public static final By COMPONENT_TITLE = By.xpath("//h6[@class=\"component-title\"]");
+	public static final By DRP_DOWN_BTN = By.xpath("//mat-select[@role=\"combobox\"]"); // it selects both dropdown on																					// the from
+
+	// Add user Button
+	public static final By USR_TYPE = By.xpath("//mat-select[@formcontrolname=\"roleId\"]");
+	public static final By FORM_INPUT = By.xpath("//input"); // it will gives the items that are in the form all 8
+
+	// Update Profile Section
+	public static final By PROF_BTN = By.xpath("//button[text()='Update User Profile']");
+	
+	// Submit Button 
+	public static final By SUBMIT_BTN = By.xpath("//button[text()=' Submit for Approval']");
 }
