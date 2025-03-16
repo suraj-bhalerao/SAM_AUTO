67d7010ec99d52276c211f0d447bb690ed3b552b
surajB2024 <surajbhalerao2024@gmail.com>
2025-03-16 12:29:25 +0530

    Test Base

---
+++ src/main/java/com/aepl/sam/base/TestBase.java
@@ -0,0 +1,95 @@
+package com.aepl.sam.base;
+
+import java.time.Duration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.asserts.SoftAssert;
+
+import com.aepl.sam.actions.MouseActions;
+import com.aepl.sam.constants.Constants;
+import com.aepl.sam.pages.LoginPage;
+import com.aepl.sam.utils.ConfigProperties;
+import com.aepl.sam.utils.WebDriverFactory;
+
+public class TestBase {
+
+	protected static WebDriver driver;
+	protected static WebDriverWait wait;
+	protected static SoftAssert softAssert;
+	protected static MouseActions action;
+	protected static LoginPage loginPage;
+
+	protected final Logger logger = LogManager.getLogger(TestBase.class);
+
+	@BeforeSuite
+	public void setUp() {
+		if (driver == null) {
+			ConfigProperties.initialize("qa");
+			String browserType = ConfigProperties.getProperty("browser");
+
+			System.out.println("Setting up WebDriver for " + browserType + " browser.");
+			driver = WebDriverFactory.getWebDriver(browserType);
+
+			if (driver == null) {
+				throw new RuntimeException("WebDriver initialization failed. Driver is null.");
+			}
+
+			wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+			driver.manage().window().maximize();
+			driver.get(Constants.BASE_URL);
+
+			System.out.println("Navigated to: " + Constants.BASE_URL);
+
+			loginPage = new LoginPage(driver);
+			action = new MouseActions(driver);
+			softAssert = new SoftAssert();
+
+			String currentClassName = this.getClass().getSimpleName();
+			if (!currentClassName.equals("LoginPageTest")) {
+				login();
+			}
+		}
+	}
+
+	@BeforeMethod
+	public void zoomChrome() {
+		if (driver != null) {
+			((JavascriptExecutor) driver).executeScript("document.body.style.zoom='80%'");
+		} else {
+			System.out.println("Zoom not applied as driver is null.");
+		}
+	}
+
+	@AfterSuite
+	public void tearDown() {
+		if (driver != null) {
+			System.out.println("Logging out and closing the browser after test suite execution.");
+
+			// Logging out and quitting i.e closing driver after use.
+			logout();
+			driver.quit();
+			driver = null;
+		} else {
+			System.out.println("Driver is already null; no browser to close.");
+		}
+	}
+
+	// Login Helper Function
+	public void login() {
+		loginPage.enterUsername(ConfigProperties.getProperty("username"))
+				.enterPassword(ConfigProperties.getProperty("password")).clickLogin();
+	}
+
+	// Logout Helper Function
+	public void logout() {
+		loginPage.clickLogout();
+	}
+}
