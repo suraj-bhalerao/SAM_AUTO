cb27c434b39ffe4d785cdfc1e50e959116378e9b
surajB2024 <surajbhalerao2024@gmail.com>
2025-04-06 20:23:21 +0530

    staterte working on the fota, device details and live analysis page

--- src/test/java/com/aepl/sam/tests/DeviceDetailsPageTest.java
+++ src/test/java/com/aepl/sam/tests/DeviceDetailsPageTest.java
@@ -1,5 +1,23 @@
 package com.aepl.sam.tests;
 
-public class DeviceDetailsPageTest {
+import org.testng.annotations.BeforeClass;
 
+import com.aepl.sam.base.TestBase;
+import com.aepl.sam.pages.DeviceDetailsPage;
+import com.aepl.sam.utils.CommonMethods;
+import com.aepl.sam.utils.ExcelUtility;
+
+public class DeviceDetailsPageTest extends TestBase{
+	private DeviceDetailsPage deviceDetails;
+	private ExcelUtility excelUtility;
+	private CommonMethods comm;
+	
+	@BeforeClass
+	public void setUp() {
+		super.setUp();
+		this.deviceDetails = new DeviceDetailsPage();
+		this.comm = new CommonMethods(driver, wait);
+		this.excelUtility = new ExcelUtility();
+		excelUtility.initializeExcel("Device_Details_Test");
+	}
 }
--- src/test/java/com/aepl/sam/tests/FotaPageTest.java
+++ src/test/java/com/aepl/sam/tests/FotaPageTest.java
@@ -1,27 +1,25 @@
 package com.aepl.sam.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 import com.aepl.sam.base.TestBase;
 import com.aepl.sam.pages.FotaPage;
+import com.aepl.sam.utils.CommonMethods;
 import com.aepl.sam.utils.ExcelUtility;
 
 public class FotaPageTest extends TestBase {
 	private FotaPage fota;
 	private ExcelUtility excelUtility;
+	private CommonMethods comm;
 
 	@BeforeClass
 	public void setUp() {
 		super.setUp();
 		this.fota = new FotaPage(driver, wait, action);
+		this.comm = new CommonMethods(driver, wait);
 		this.excelUtility = new ExcelUtility();
 		excelUtility.initializeExcel("FOTA_Test");
 	}
-
-	@Test
-	public void test() {
-		Assert.fail("Failed");
-	}
+	
+	
 }
