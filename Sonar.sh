a04ac2f37ee425d0eb76c66dcbf59ffad9421a29
suraj-bhalerao <surajbhalerao2024@gmail.com>
2025-07-20 16:08:51 +0530

    new

--- Sonar.sh
+++
@@ -1,5 +0,0 @@
-mvn clean verify sonar:sonar \
-  -Dsonar.projectKey=com.aepl.sam:Sampark \
-  -Dsonar.projectName='Sampark' \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.token=sqp_dd0578fbf871a853979cb5c520ebb861959667f5
