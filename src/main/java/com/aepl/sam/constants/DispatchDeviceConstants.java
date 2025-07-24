3a3c8175c909ea80915134914693e3db60881798
JDhananjay79 <jagtapdhananjay79@gmail.com>
2025-07-24 11:48:39 +0530

    Dispatched changes

---
+++ src/main/java/com/aepl/sam/constants/DispatchDeviceConstants.java
@@ -0,0 +1,12 @@
+package com.aepl.sam.constants;
+
+public interface DispatchDeviceConstants {
+
+	
+	String TC_LOGO = "Verify Company Logo on Webpage";
+	String TC_PAGE_TITLE = "Verify Page Title on Webpage";
+	String TC_NAV_BAR_LINK = "Test Navigation to Device Utility Tab";
+	
+	String LOGO_DISPLAYED = "Logo Displayed";
+	String LOGO_NOT_DISPLAYED = "Logo Not Displayed";
+}
